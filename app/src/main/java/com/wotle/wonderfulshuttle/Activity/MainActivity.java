07e9cf7588492cb7bd793f4c879f38ef65d78b01
bayprase <bayuprasetyo.bp11@gmail.com>
2022-08-09 19:01:32 +0700

    Modif struktur folder

--- app/src/main/java/com/wotle/wonderfulshuttle/Activity/MainActivity.java
+++ app/src/main/java/com/wotle/wonderfulshuttle/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.wotle.wonderfulshuttle;
+package com.wotle.wonderfulshuttle.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.wotle.wonderfulshuttle.R;
 
 public class MainActivity extends AppCompatActivity {
 
