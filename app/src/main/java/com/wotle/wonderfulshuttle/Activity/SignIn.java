8b588c8a4bb54c92eec2d65ec544c54cfe71a9db
b4yuPraze <20190810011@uniku.ac.id>
2022-08-13 03:12:09 +0700

    Tambah activity login

---
+++ app/src/main/java/com/wotle/wonderfulshuttle/Activity/SignIn.java
@@ -0,0 +1,15 @@
+package com.wotle.wonderfulshuttle.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wotle.wonderfulshuttle.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
