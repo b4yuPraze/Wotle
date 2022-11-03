a6bc899cb8fed6a00839744162704ca7da2e851d
adimift <adimiftahul98@gmail.com>
2022-11-03 20:31:00 +0700

    test

--- app/src/main/java/com/wotle/wotle/Customer/EmergencyContact.java
+++ app/src/main/java/com/wotle/wotle/Customer/EmergencyContact.java
@@ -2,33 +2,63 @@ package com.wotle.wotle.Customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.wotle.wotle.R;
 
 public class EmergencyContact extends AppCompatActivity {
-    Button Buttontambah;
+    private Button showDialogButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_contact);
+        showDialogButton = (Button) findViewById(R.id.button_show_dialog);
 
-
-        Buttontambah = findViewById(R.id.tambahKontak);
-        Buttontambah.setOnClickListener(new View.OnClickListener() {
-
+        showDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showCustomDialog();
+            }
+        });
+    }
 
-                Intent i = new Intent(getApplicationContext(),Form_EmergencyContact.class);
-                startActivity(i);
-                finish();
+    private void showCustomDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setTitle("Add person");
+        dialog.setContentView(R.layout.activity_form_emergency_contact);
+        dialog.setCanceledOnTouchOutside(false);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        dialog.getWindow().setLayout((6 * width) / 7, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        Button cancelButton = (Button) dialog.findViewById(R.id.button_cancel);
+        Button saveButton = (Button) dialog.findViewById(R.id.button_save);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(EmergencyContact.this, "Data saved", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
             }
         });
 
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        //Menampilkan custom dialog
+        dialog.show();
     }
-}
+
+    }
